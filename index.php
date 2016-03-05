b4ead86713c38b4a9f5403da618fd94cf60d6d98
isabel <isabelhuaycho@gmail.com>
2016-03-05 00:57:48 -0500

    segundo mundo

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title> proyecto</title>
+	<meta charset="utf-8">
+	
+	<link rel="stylesheet" type="text/css"href="estilo.css" >
+</head>
+<body>
+<div class="contenedor">
+<na>
+<ul class="menu lista">
+
+
+
+<nav>
+
+	<ul class="menu-lista">
+		<li class="menu-item"><a href="" class="menu-link">INICIO</a></li>
+		<li class="menu-item"><a href="" class="menu-link">GALERIA</a></li>
+		<li class="menu-item"><a href="" class="menu-link">BOLG</a></li>
+		<li class="menu-item"><a href="" class="menu-link">ANUNCIOS</a></li>
+		<li class="menu-item"><a href="" class="menu-link">CONTACTENOS</a></li>
+
+
+		
+	</ul>
+</nav>
+
+
+
+<div class="slider">
+<img  cls="img" src="iladia.jpg" alt ="mi imagen" height="520" width="990">
+
+<img clas ="img" src="isa.jpg" alt="mi imagen favorita" height="225" width="225">
+
+<img clas ="img" src="inet.jpg"alt="mi imagen favorita" height="225" width="225">
+
+<img clas ="img" src="isabel.jpg"alt="mi imagen favorita" height="225" width="225">
+<img clas ="img" src="ta.PNG"alt="mi imagen favorita" height="225" width="225">
+
+
+</body>
+</html>
